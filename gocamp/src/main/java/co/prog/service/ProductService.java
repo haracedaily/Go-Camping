3268622aa3f93bc0bf7f35f5002cc258cd986bf9
daekyu <daeku7820566@gmail.com>
2023-03-17 18:06:34 +0900

    0317

--- gocamp/src/main/java/co/prog/service/ProductService.java
+++ gocamp/src/main/java/co/prog/service/ProductService.java
@@ -3,7 +3,10 @@ package co.prog.service;
 import java.util.List;
 
 import co.prog.vo.ProductVO;
+import co.prog.vo.SearchCondition;
 
 public interface ProductService {
-	public List<ProductVO> products();
+	public List<ProductVO> products(SearchCondition con);
+
+	
 }
--- gocamp/src/main/java/co/prog/service/ProductServiceMybatis.java
+++ gocamp/src/main/java/co/prog/service/ProductServiceMybatis.java
@@ -7,23 +7,17 @@ import org.apache.ibatis.session.SqlSession;
 import co.prog.common.DataSource;
 import co.prog.mapper.ProductMapper;
 import co.prog.vo.ProductVO;
+import co.prog.vo.SearchCondition;
 
-public class ProductServiceMybatis implements ProductService{
+public class ProductServiceMybatis implements ProductService {
 
-	
-	SqlSession sqlSession =DataSource.getInstance().openSession(true);
-	ProductMapper mapper= sqlSession.getMapper(ProductMapper.class);
-	
-	
-	
-	
-	
-	
-	
-	
+	SqlSession sqlSession = DataSource.getInstance().openSession(true);
+	ProductMapper mapper = sqlSession.getMapper(ProductMapper.class);
+
 	@Override
-	public List<ProductVO> products() {
-		return mapper.productList();
+	public List<ProductVO> products(SearchCondition con) {
+		return mapper.productList(con);
 	}
+
 
 }
